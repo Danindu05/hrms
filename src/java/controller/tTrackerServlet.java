8b435ada740846f2e760a688f54a3dd028eafc54
irushinw <irushinawanjana@gmail.com>
2025-05-09 10:56:02 +0530

    ttracker servlet

--- src/java/controller/tTrackerServlet.java
+++ src/java/controller/tTrackerServlet.java
@@ -2,12 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package controller;
 
-/**
- *
- * @author Irushi Weerasinghe
- */
-public class tTrackerServlet {
-    
+import dao.tTrackerDAO;
+import model.tTracker;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class tTrackerServlet extends HttpServlet {
+    private tTrackerDAO dao;
+
+    public void init() {
+        dao = new tTrackerDAO();
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int empId = Integer.parseInt(req.getParameter("id"));
+        tTracker data = dao.getEmployeeData(empId);
+
+        req.setAttribute("tracker", data);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("tTracker.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
